c3cafaa2dbc621f0b53b9c2a2dcc51400e1ba35c
Rob Hansen <rob@hansen.engineering>
2025-03-20 00:36:35 -0400

    Works properly on MSVC again.

--- build.ps1
+++ build.ps1
@@ -3,9 +3,16 @@
 Remove-Item -Recurse -Force build
 New-Item -ItemType Directory -Force -Path build
 Set-Location -Path build
-cmake -S .. -B . -G "Visual Studio 17 2022"
-MsBuild.exe djinni.sln /t:djinni_example /p:Platform="x64" /p:Configuration=Release
-Copy-Item src\Release\djinni_example.exe -Destination ..\djinni_example.exe
+$output_file = "djinni_example"
+if ($IsWindows) {
+    $output_file += ".exe"
+    cmake -S .. -B . -G "Visual Studio 17 2022"
+    MsBuild.exe djinni.sln /t:djinni_example /p:Platform="x64" /p:Configuration=Release
+    Copy-Item src\Release\$output_file -Destination ..\$output_file
+} else {
+    cmake -S .. -B . -D CMAKE_BUILD_TYPE=Release
+    make -j8
+}
 Set-Location -Path ..
 Remove-Item -Recurse -Force build
 New-Item -ItemType Directory -Path djinni
@@ -13,4 +20,4 @@ Copy-Item src\djinni.h -Destination djinni\djinni.h
 Copy-Item src\djinni -Destination djinni -Recurse
 Compress-Archive -Path djinni -DestinationPath djinni.zip
 Remove-Item -Recurse -Force djinni
-Get-ChildItem djinni_example.exe
+Get-ChildItem djinni*
